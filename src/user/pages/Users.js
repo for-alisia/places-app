14c95451c72b86e58dc3bc80f6e85d7a9f38f44d
Elena Romanova <for-alisia@yandex.ru>
2020-05-14 12:00:07 +0500

    Added places page

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -11,6 +11,20 @@ const Users = () => {
                 'https://images.unsplash.com/photo-1588887563897-7809995fe9b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=802&q=80',
             places: 3,
         },
+        {
+            id: 'u2',
+            name: 'Jason',
+            image:
+                'https://images.unsplash.com/photo-1584202532967-6390de14ecac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=693&q=80',
+            places: 5,
+        },
+        {
+            id: 'u3',
+            name: 'Sandy',
+            image:
+                'https://images.unsplash.com/photo-1489701714346-794d8674a788?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+            places: 8,
+        },
     ];
 
     return <UsersList items={USERS} />;
